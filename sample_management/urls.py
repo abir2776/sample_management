7beebcf9595f99226ee5cbcefe0477d48fafe61c
abir2776 <osmangoni255@gmail.com>
2025-12-04 17:41:36 +0600

    fixed css not showing issue

--- sample_management/urls.py
+++ sample_management/urls.py
@@ -63,3 +63,5 @@ urlpatterns = [
     path("api/v1/core/logs/", include("core.rest.urls.activitylog")),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
