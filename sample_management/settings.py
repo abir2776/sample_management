044d4285142d5168cfd89fd5c5d3b3976fd8a2cc
abir2776 <osmangoni255@gmail.com>
2025-12-22 16:52:16 +0600

    changed celery broker url

--- sample_management/settings.py
+++ sample_management/settings.py
@@ -175,8 +175,8 @@ CSRF_TRUSTED_ORIGINS = [
 ]
 
 # Celery Configuration Options
-CELERY_BROKER_URL = "redis://localhost:6379/0"
-CELERY_RESULT_BACKEND = "redis://localhost:6379/0"
+CELERY_BROKER_URL = "redis://127.0.0.1:6379/0"
+CELERY_RESULT_BACKEND = "redis://127.0.0.1:6379/0"
 
 # Optional settings
 CELERY_ACCEPT_CONTENT = ["json"]
